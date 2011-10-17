8e06609593a82dddf2d8f02b7c431d84d34adb2e
David Amian <damian@emergya.com>
2011-10-17 17:09:23 +0200

    Added config to use with DistUtils

---
+++ pnl/pnlconfig.py
@@ -0,0 +1,75 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (c) 2011 Junta de Andalucia
+#
+# Authors:
+#    David Amian <damian at emergya.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
+
+# THIS IS Hans CONFIGURATION FILE
+# YOU CAN PUT THERE SOME GLOBAL VALUE
+# Do not touch unless you know what you're doing.
+# you're warned :)
+
+__all__ = [
+    'project_path_not_found',
+    'get_data_file',
+    'get_data_path',
+    ]
+
+# Where your project will look for your data (for instance, images and ui
+# files). By default, this is ../data, relative your trunk layout
+__pnl_data_directory__ = '/usr/local/share/pnl/'
+__license__ = ''
+
+import os
+
+import gettext
+from gettext import gettext as _
+gettext.textdomain('pnl')
+
+class project_path_not_found(Exception):
+    """Raised when we can't find the project directory."""
+
+
+def get_data_file(*path_segments):
+    """Get the full path to a data file.
+
+    Returns the path to a file underneath the data directory (as defined by
+    `get_data_path`). Equivalent to os.path.join(get_data_path(),
+    *path_segments).
+    """
+    return os.path.join(get_data_path(), *path_segments)
+
+
+def get_data_path():
+    """Retrieve hans data path
+
+    This path is by default <pnl_lib_path>/../data/ in trunk
+    and /usr/share/pnl in an installed version but this path
+    is specified at installation time.
+    """
+
+    # Get pathname absolute or relative.
+    path = os.path.join(
+        os.path.dirname(__file__), __pnl_data_directory__)
+
+    abs_data_path = os.path.abspath(path)
+    if not os.path.exists(abs_data_path):
+        raise project_path_not_found
+
+    return abs_data_path
---
+++ pnl/utils.py
@@ -0,0 +1,52 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (c) 2011 Junta de Andalucia
+#
+# Authors:
+#    Antonio Hernández <ahernandez at emergya.com>
+#    David Amian <damian at emergya.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
+
+import pynotify
+import os
+import gtk
+import sys
+from pnl import _
+from pnl.pnlconfig import get_data_path
+
+def notify(title, message, icon, timeout=pynotify.EXPIRES_DEFAULT, transient=True):
+
+    # Desktop Notifications Specification: http://www.galago-project.org/specs/notification/0.9/index.html
+
+    if not pynotify.init("PnL " + _("Notifications")):
+        return
+
+    notify = pynotify.Notification(title, message)
+    notify.set_icon_from_pixbuf(icon)
+    notify.set_category('device.added')
+    notify.set_urgency(pynotify.URGENCY_LOW)
+    notify.set_hint('transient', transient)
+    notify.set_timeout(timeout)
+
+    if not notify.show():
+        print _("Failed to send notification")
+
+def get_script_user_path():
+    return os.path.join(os.path.expanduser('~'), ".pnl/scripts/")
+
+def get_system_user_path():
+    return os.path.join(get_data_path(), "scripts")
