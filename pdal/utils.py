cb5f5ba4cf0b9afb0c61b8acb1f4284729695343
David Amian <damian@emergya.com>
2011-10-17 19:07:38 +0200

    Added some utils functions

--- pdal/utils.py
+++ pdal/utils.py
@@ -28,6 +28,18 @@ import sys
 from pdal import _
 from pdal.pdalconfig import get_data_path
 
+DEFAULT_ICON_INTERFACE = 'media/default-icon-interface.svg'
+DEFAULT_ICON_DEVICE = 'media/default-icon-device.svg'
+DEFAULT_ICON_SIZE = 48
+
+
+def get_default_icon_interface():
+    return os.path.join(get_data_path(), DEFAULT_ICON_INTERFACE)
+
+def get_default_icon_device():
+    return os.path.join(get_data_path(), DEFAULT_ICON_DEVICE)
+
+
 def notify(title, message, icon, timeout=pynotify.EXPIRES_DEFAULT, transient=True):
 
     # Desktop Notifications Specification: http://www.galago-project.org/specs/notification/0.9/index.html
@@ -50,3 +62,22 @@ def get_script_user_path():
 
 def get_system_user_path():
     return os.path.join(get_data_path(), "scripts")
+
+
+def get_pixbuf_from_file(file_name, icon_size=DEFAULT_ICON_SIZE, flags=gtk.gdk.INTERP_BILINEAR):
+    if not os.path.exists(file_name):
+        return None
+    image = gtk.Image()
+    image.set_from_file(file_name)
+    pixbuf = image.get_pixbuf()
+    pixbuf = pixbuf.scale_simple(icon_size, icon_size, flags)
+    return pixbuf
+
+def get_theme_icon_path(icon_name, icon_size=DEFAULT_ICON_SIZE, flags=0):
+    icon_theme = gtk.icon_theme_get_default()
+    icons = icon_theme.list_icons()
+    if not icon_name in icons:
+        return None
+    file_name = icon_theme.lookup_icon(icon_name, icon_size, flags).get_filename()
+    return file_name
+
