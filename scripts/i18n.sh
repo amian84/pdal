b1687906e49c9c60089f17b027f9780f30b0db13
David Amian <damian@emergya.com>
2011-10-20 15:06:43 +0200

    Added script to update i18n

---
+++ scripts/i18n.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+lang=$1
+appname="pdal"
+
+if [ "" == "$lang" ]; then
+    lang="es"
+fi
+
+podir="../po"
+potfilesin="${podir}/POTFILES.in"
+potfile="${podir}/${appname}.pot"
+pofile="${podir}/${lang}.po"
+pofilemerged="${podir}/${lang}.merged.po"
+mofile="${podir}/${appname}.mo"
+
+find .. -type f -name "*.py" > $potfilesin
+
+xgettext --language=Python --keyword=_ --output=$potfile -f $potfilesin
+
+if [ ! -f $pofile ]; then
+
+    msginit --input=$potfile --locale=es_ES --output-file $pofile
+
+else
+
+    msgmerge $pofile $potfile > $pofilemerged
+    mv $pofilemerged $pofile
+
+fi
