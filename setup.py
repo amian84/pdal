e349d4801d53676391ca5a2c5ba8b62f103ad98a
David Amian <damian@emergya.com>
2011-12-05 17:37:23 +0100

    Added install commando to setup.py

--- setup.py
+++ setup.py
@@ -1,60 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, os, platform
-from distutils.core import setup, Command
-from DistUtilsExtra.command import *
-import glob
+import sys, os, glob
+
+try:
+    import DistUtilsExtra.auto
+    from distutils.core import setup, Command
+    from DistUtilsExtra.command import *
+except ImportError:
+    print >> sys.stderr, 'To build firstboot you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
+assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
 
 # Get current Python version
-python_version = platform.python_version_tuple()
 
 # Setup the default install prefix
-prefix = sys.prefix
-oldvalue=None
+#prefix = sys.prefix
+#oldvalue=None
 # Check our python is version 2.6 or higher
-if python_version[0] >= 2 and python_version[1] >= 6:
+#if python_version[0] >= 2 and python_version[1] >= 6:
     ## Set file location prefix accordingly
-    prefix = '/usr/local'
+ #   prefix = '/usr/local'
 
 # Get the install prefix if one is specified from the command line
-for arg in sys.argv:
-    if arg.startswith('--prefix='):
-        prefix = arg[9:]
-        prefix = os.path.expandvars(prefix)
+#for arg in sys.argv:
+#    if arg.startswith('--prefix='):
+#        prefix = arg[9:]
+#        prefix = os.path.expandvars(prefix)
 
-for filename in ['udev-rules/99-pdal.rules']:
-    infile = open(filename + '.in', 'r')
-    data = infile.read().replace('@PREFIX@', prefix)
-    infile.close()
+def update_rules(prefix):
 
-    outfile = open(filename, 'w')
-    outfile.write(data)
-    outfile.close()
+    for filename in ['udev-rules/99-pdal.rules']:
+        infile = open(filename + '.in', 'r')
+        data = infile.read().replace('@PREFIX@', prefix)
+        infile.close()
 
+        outfile = open(filename, 'w')
+        outfile.write(data)
+        outfile.close()
 
-def update_data_path(prefix, oldvalue_data=None, oldvalue_prefix=None):
+def update_config(values={}):
+
+    oldvalues = {}
     try:
-        fin = file('pdal/pdalconfig.py', 'r')
+        fin = file('firstboot_lib/firstbootconfig.py', 'r')
         fout = file(fin.name + '.new', 'w')
 
         for line in fin:
             fields = line.split(' = ') # Separate variable from value
-            if fields[0] == '__pdal_data_directory__':
-                # update to prefix, store oldvalue
-                if not oldvalue_data:
-                    oldvalue_data = fields[1]
-                    line = "%s = '%s'\n" % (fields[0], prefix)
-                else: # restore oldvalue
-                    line = "%s = %s" % (fields[0], oldvalue_data)
-            
-            if fields[0] == '__pdal_prefix__':
-                if not oldvalue_prefix:
-                    oldvalue_prefix = fields[1]
-                    line = "%s = '%s'\n" % (fields[0], prefix)
-                else: # restore oldvalue
-                    line = "%s = %s" % (fields[0], oldvalue_prefix)
-
+            if fields[0] in values:
+                oldvalues[fields[0]] = fields[1].strip()
+                line = "%s = %s\n" % (fields[0], values[fields[0]])
             fout.write(line)
 
         fout.flush()
@@ -62,12 +58,71 @@ def update_data_path(prefix, oldvalue_data=None, oldvalue_prefix=None):
         fin.close()
         os.rename(fout.name, fin.name)
     except (OSError, IOError), e:
-        print ("ERROR: Can't find pdal/pdalconfig.py")
+        print ("ERROR: Can't find firstboot_lib/firstbootconfig.py")
         sys.exit(1)
+    return oldvalues
+
+
+class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
+    def run(self):
+        values = {'__pdal_data_directory__': "'%s'" % (self.prefix + '/share/pdal/'),
+                  '__license__': "'%s'" % self.distribution.get_license(),
+                  '__firstboot_prefix__': "'%s'" % self.prefix}
+        previous_values = update_config(values)
+        update_rules(self.prefix + '/share/pdal/')
+        DistUtilsExtra.auto.install_auto.run(self)
+        update_config(previous_values)
+
+
+class Clean(Command):
+    description = "custom clean command that forcefully removes dist/build directories and update data directory"
+    user_options = []
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
+        os.system('rm -rf ./build ./dist')
+        os.system('rm -rf udev-rules/99-pdal.rules')
+
+
+
+#def update_data_path(prefix, oldvalue_data=None, oldvalue_prefix=None):
+#    try:
+#        fin = file('pdal/pdalconfig.py', 'r')
+#        fout = file(fin.name + '.new', 'w')
+#
+#        for line in fin:
+#            fields = line.split(' = ') # Separate variable from value
+#            if fields[0] == '__pdal_data_directory__':
+#                # update to prefix, store oldvalue
+#                if not oldvalue_data:
+#                    oldvalue_data = fields[1]
+#                    line = "%s = '%s'\n" % (fields[0], prefix)
+#                else: # restore oldvalue
+#                    line = "%s = %s" % (fields[0], oldvalue_data)
+#            
+#            if fields[0] == '__pdal_prefix__':
+#                if not oldvalue_prefix:
+#                    oldvalue_prefix = fields[1]
+#                    line = "%s = '%s'\n" % (fields[0], prefix)
+#                else: # restore oldvalue
+#                    line = "%s = %s" % (fields[0], oldvalue_prefix)
+#
+#            fout.write(line)
+#
+#        fout.flush()
+#        fout.close()
+#        fin.close()
+#        os.rename(fout.name, fin.name)
+#    except (OSError, IOError), e:
+#        print ("ERROR: Can't find pdal/pdalconfig.py")
+#        sys.exit(1)
 #    return (oldvalue_data, oldvalue_prefix)
 
 #oldvalue=update_data_path(prefix + '/share/pdal/')
-update_data_path(prefix + '/share/pdal/')
+#update_data_path(prefix + '/share/pdal/')
 
 
 #class Clean(Command):
@@ -94,23 +149,17 @@ update_data_path(prefix + '/share/pdal/')
 #    outfile.write(data)
 #    outfile.close()
 
+
 setup(
         name='pdal2',
-        version='0.3.1',
-        description='PDAL (Pluggable Device Action Launcher).',
+        version='0.3.2',
+        license='GPL-2',
         author='David Amian Valle',
         author_email='damian@emergya.com',
-        url='https://launchpad.net/pdal',
+        description='PDAL (Pluggable Device Action Launcher)',
+        url='https://github.com/amian84/pdal',
 
-        classifiers=[
-            'Development Status :: 0.2 - Alpha',
-            'Environment :: Desktop Environment',
-            'License :: OSI Approved :: GNU General Public License (GPL)',
-            'Operating System :: POSIX',
-            'Programming Language :: Python',
-	        'Topic :: Utilities'
-        ],
-        
+
         keywords = ['python', 'udev', 'gnome'],
 
         packages = ['pdal'], 
@@ -125,10 +174,10 @@ setup(
             ('share/pdal/media', glob.glob('data/media/*')),
         ],
         cmdclass = { 
-                 
+            'install': InstallAndUpdateDataDirectory,                 
             "build" : build_extra.build_extra,
             "build_i18n" :  build_i18n.build_i18n,
-            "clean": clean_i18n.clean_i18n,#Clean],
+            "clean": [clean_i18n.clean_i18n, Clean],
             
         }
 )
